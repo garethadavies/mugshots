3019facb5bc42e4c4a5301d09b75d5629a446938
Gareth Davies <garethaneurindavies@gmail.com>
2013-09-11 21:02:49 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mugshots::Application.config.secret_token = 'eb0d72694d325e71fdbd197e1ef0b987b2441d2c83becd0b758b620296ae3343cee17aa14d58a83c416533f800a54ca35cc1255e1d607fa8488ca7c000b2bd20'
